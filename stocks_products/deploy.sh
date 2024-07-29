0a53e0f3661b0f38fdc3bd8435e496ba04f625a5
_lyovka_ <50871926+ivanovlev02@users.noreply.github.com>
2024-07-29 17:15:25 +0700

    Update deploy.sh

--- stocks_products/deploy.sh
+++ stocks_products/deploy.sh
@@ -4,6 +4,7 @@ cd /home/lev/Netology/stocks_products
 git pull origin main
 source ../net_env/bin/activate
 pip install -r requirements.txt
+python3 manage.py makemigrations
 python3 manage.py migrate
 deactivate
 sudo systemctl restart gunicorn
