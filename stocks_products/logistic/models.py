73b6202fa6cdcb6153a2775a82600650b210428d
_lyovka_ <50871926+ivanovlev02@users.noreply.github.com>
2024-07-29 18:14:32 +0700

    Update models.py

--- stocks_products/logistic/models.py
+++ stocks_products/logistic/models.py
@@ -36,7 +36,7 @@ class StockProduct(models.Model):
 
     class Meta:
         verbose_name = 'склад'
-        verbose_name_plural = 'Остатки'
+        verbose_name_plural = 'Остатки складов'
         ordering = ['stock']
 
     stock = models.ForeignKey(
